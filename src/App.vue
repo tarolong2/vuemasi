4fca1c32519329e57afe77753083b16c916bbcc1
tarolong2 <tarolong2@gmail.com>
2022-09-02 10:36:08 +0900

    버전완료

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,12 @@ import VisualView from '@/components/VisualView.vue'
 import ItemView from '@/components/ItemView.vue'
 import FooterView from '@/components/FooterView.vue'
 
+// 공통으로 관리되는 state 를 참조한다.
+// 여기서는 actions 를 호출하는 용도로 사용.
+// vuex 를 참조하는 객체를 접근하려고 하면 
+// 아래 구문으로 접근한다.
+import { useStore } from 'vuex'
+
 export default {
   name: 'App',
   components: {
@@ -31,6 +37,14 @@ export default {
     FooterView
   },
   setup() {
+    // 외부에 있는 menu.json 파일을 불러오기 위해서
+    // actions 를 활용한다.
+    const store = useStore();
+
+    // actions 의 메소드(function) 를 사용할때
+    // store.dispatch('메소드명')
+    // console.log('step 1 : dispatch');
+    store.dispatch('fetchMenudata');
     return { 
     }
   }
